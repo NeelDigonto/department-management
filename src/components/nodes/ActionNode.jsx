e7e0519b8d9fb9e1c845a35b5b3915f3f9f5bc40
fanthomless <digonto.pvt.2021@gmail.com>
2021-10-22 01:32:29 +0530

    Changed formcontrollabels in action nodes to toggle buttons, looks better now

--- src/components/nodes/ActionNode.jsx
+++ src/components/nodes/ActionNode.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Box, FormControlLabel, Checkbox } from "@mui/material";
+import { Box, ToggleButton, ToggleButtonGroup } from "@mui/material";
 import FilterListIcon from "@mui/icons-material/FilterList";
-import FilterListOutlinedIcon from "@mui/icons-material/FilterListOutlined";
 import SortIcon from "@mui/icons-material/Sort";
-import SortOutlinedIcon from "@mui/icons-material/SortOutlined";
 import AddToQueueIcon from "@mui/icons-material/AddToQueue";
-import AddToQueueOutlinedIcon from "@mui/icons-material/AddToQueueOutlined";
+import PrintIcon from "@mui/icons-material/Print";
 
 import { INPUT_TYPE } from "../../data/types/types";
 
@@ -24,99 +22,68 @@ const ActionNode = ({
   displayRef,
   valueLastUpdatedRef,
 }) => {
-  const handleFilterToggle = () => {
-    valueLastUpdatedRef.current = new Date();
-    setFilter((oldState) => {
-      if (oldState) {
-        if (field.input_type !== INPUT_TYPE.FILE)
-          delete toFilterRef.current[`${categoryName}.${field.db_field}`];
-        else
-          delete toFilterRef.current[`${categoryName}.${field.db_field}.fname`];
-      } else {
-        if (field.input_type !== INPUT_TYPE.FILE)
-          toFilterRef.current[`${categoryName}.${field.db_field}`] = true;
-        else
-          toFilterRef.current[`${categoryName}.${field.db_field}.fname`] = true;
-      }
+  const [formats, setFormats] = React.useState(() => [
+    /*     "filter",
+    "sort",
+    "display", */
+  ]);
 
-      return !oldState;
-    });
-  };
-
-  const handleSortToggle = () => {
+  const handleFormat = (event, newFormats) => {
+    setFormats(newFormats);
     valueLastUpdatedRef.current = new Date();
-    setSort((oldState) => {
-      //if (oldState) delete sortRef.current[`${categoryName}.${field.db_field}`];
-      return !oldState;
-    });
-  };
 
-  const handleDisplayToggle = () => {
-    valueLastUpdatedRef.current = new Date();
-    setDisplay((oldState) => {
-      if (oldState)
-        delete displayRef.current[`${categoryName}.${field.db_field}`];
-      else displayRef.current[`${categoryName}.${field.db_field}`] = 1;
-      return !oldState;
-    });
+    // filter update
+    if (newFormats.includes("filter")) {
+      setFilter(true);
+      if (field.input_type !== INPUT_TYPE.FILE)
+        toFilterRef.current[`${categoryName}.${field.db_field}`] = true;
+      else
+        toFilterRef.current[`${categoryName}.${field.db_field}.fname`] = true;
+    } else {
+      setFilter(false);
+      if (field.input_type !== INPUT_TYPE.FILE)
+        delete toFilterRef.current[`${categoryName}.${field.db_field}`];
+      else
+        delete toFilterRef.current[`${categoryName}.${field.db_field}.fname`];
+    }
+
+    // sort update
+    if (newFormats.includes("sort")) {
+      setSort(true);
+    } else {
+      setSort(false);
+    }
+
+    // display update
+    if (newFormats.includes("display")) {
+      setDisplay(true);
+      displayRef.current[`${categoryName}.${field.db_field}`] = 1;
+    } else {
+      setDisplay(false);
+      delete displayRef.current[`${categoryName}.${field.db_field}`];
+    }
   };
 
   return (
-    <Box>
-      <FormControlLabel
-        control={
-          <Checkbox
-            icon={<FilterListOutlinedIcon />}
-            checkedIcon={<FilterListIcon />}
-            color="primary"
-            checked={filter}
-            onChange={handleFilterToggle}
-            name={field.db_field}
-          />
-        }
-        label=""
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            icon={<SortOutlinedIcon />}
-            checkedIcon={<SortIcon />}
-            color="primary"
-            checked={sort}
-            onChange={handleSortToggle}
-            name={field.db_field}
-          />
-        }
-        label=""
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={display}
-            icon={<AddToQueueOutlinedIcon />}
-            checkedIcon={<AddToQueueIcon />}
-            color="primary"
-            onChange={handleDisplayToggle}
-            name={field.db_field}
-          />
-        }
-        label=""
-      />
+    <Box pt={2}>
+      <ToggleButtonGroup
+        size="small"
+        value={formats}
+        onChange={handleFormat}
+        aria-label="search parameters"
+      >
+        <ToggleButton value="filter" aria-label="filter">
+          <FilterListIcon />
+        </ToggleButton>
+        <ToggleButton value="sort" aria-label="sort">
+          <SortIcon />
+        </ToggleButton>
+        <ToggleButton value="display" aria-label="display">
+          <PrintIcon />
+        </ToggleButton>
+      </ToggleButtonGroup>
     </Box>
   );
 };
 
 export default ActionNode;
-
-/*<FormControlLabel
-        control={
-          <Checkbox
-            checked={display}
-            onChange={handleDisplayToggle}
-            name={field.db_field}
-            color="primary"
-          />
-        }
-        //label={"Display"}
-        label={<AddToQueueIcon />}
-      ></FormControlLabel>*/
